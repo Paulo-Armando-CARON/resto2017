4934a7c620c22873d6c6ce3eca7106b7414fdba7
Paulo <paulo@gmail>
2018-01-16 16:38:13 +0100

    add4

--- www/index.php
+++ www/index.php
@@ -1,8 +1,7 @@
 <?php include_once"inc/sql.php";
 
 
-$req=$pdo->query('SELECT `id_product`,`title_product`,`description`,`price_ttc`
-FROM table_product');
+$req=$pdo->query('SELECT * FROM table_product');
 
 
 include_once 'LayoutView.phtml';
